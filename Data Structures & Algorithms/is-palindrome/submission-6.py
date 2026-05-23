ccca4e2cd743ba9d0477c716701466e717aee0c8
TaheraAhmed <68482985+TaheraAhmed@users.noreply.github.com>
2026-05-23 17:11:32 +0400

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        res=True
+        clean_string=""
+        for i in range(len(s)):
+            if s[i].isalnum():
+                clean_string=clean_string+(s[i].lower())
+        
+        start=0
+        end=len(clean_string)-1
+        if clean_string=='':
+            return res
+        while start!=end:
+            if clean_string[start]==clean_string[end]:
+                start+=1
+                end-=1  
+                if start>end:
+                    break  
+            else:
+                res=False
+                return res
+        
+        return res
+                
+             
+            
